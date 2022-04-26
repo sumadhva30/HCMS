8f9650bc996333705f26d31990b24abf57b7e5a1
Govind Balaji S <s.govindbalaji@gmail.com>
2022-04-26 18:27:47 +0530

    Add update_incident, update_schedule's

--- backend/dbaccess.py
+++ backend/dbaccess.py
@@ -2,10 +2,19 @@ import Models
 
 from pymongo import MongoClient
 
-from backend.Models import OnCallWeekly
+from backend.Models import IncidentInfo, OnCallWeekly
 client = MongoClient()
 
 db = client['HCMS_db']
 
 def search_weekly_oncall_schedule(query: OnCallWeekly):
+    pass
+
+def set_update_incident(query: IncidentInfo):
+    pass
+
+def append_incident_msgs(query: IncidentInfo):
+    pass
+
+def append_incident_notes(query: IncidentInfo):
     pass
--- backend/update.py
+++ backend/update.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from fastapi import HTTPException
 from Models import IncidentInfo
 import dbaccess
-from backend.Models import OnCallWeekly, WeeklySlot 
+from backend.Models import OnCallSpecific, OnCallWeekly, WeeklySlot 
 
 
-def check_weekly_oncall_schedule(incident: IncidentInfo):
+def check_weekly_oncall_schedule(incident: IncidentInfo) -> str:
     oncall_weekly_query = OnCallWeekly(cat = incident.cat)
     schedule = dbaccess.search_weekly_oncall_schedule(oncall_weekly_query)
     WEEKDAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
@@ -12,15 +13,27 @@ def check_weekly_oncall_schedule(incident: IncidentInfo):
 
     assert len(schedule) <= 1
     if len(schedule) == 0:
-        raise FileNotFoundError('weekly schedule not found')
+        raise HTTPException(status_code=404, detail='weekly schedule not found')
     
     weekly_slot : WeeklySlot = getattr(schedule, day)
     
     if datetime.now().hour <= 15:
         responder = weekly_slot.Fn_id
-    else
+    else:
         responder = weekly_slot.An_id
     
     return responder
 
-    
+def update_incident(incident: IncidentInfo) -> IncidentInfo:
+    if incident.msgs != None:
+        return dbaccess.append_incident_msgs(incident)
+    elif incident.notes != None:
+        return dbaccess.append_incident_notes(incident)
+    else:
+        return dbaccess.set_update_incident(incident)
+
+def update_specific_oncall_schedule(oncall_specific: OnCallSpecific) -> OnCallSpecific:
+    return dbaccess.set_specific_oncall_schedule(oncall_specific)
+
+def update_weekly_oncall_schedule(oncall_weekly: OnCallWeekly) -> OnCallWeekly:
+    return dbaccess.set_weekly_oncall_schedule(oncall_weekly)
