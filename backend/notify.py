83c7407cc0cfa387b07085bb68d47cd760a75c98
Sumadhva Sridhar <sumadhvasridhar@gmail.com>
2022-04-28 12:27:33 +0530

    Fix notifications

--- backend/notify.py
+++ backend/notify.py
@@ -2,8 +2,8 @@ import os
 import smtplib
 
 def notify_user(rec, sub: str, body: str):
-    EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
-    EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+    EMAIL_ADDRESS = "vamshi.test1234@gmail.com"
+    EMAIL_PASSWORD = "gxpokrpfarbvhugw"
 
     with smtplib.SMTP('smtp.gmail.com',587) as smtp:
         smtp.ehlo()
