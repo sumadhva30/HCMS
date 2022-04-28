c01206d257ebc58ede2ad5ff9a9aa1477454961a
Govind Balaji S <s.govindbalaji@gmail.com>
2022-04-28 19:33:59 +0530

    probably raiseTicket frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,7 +40,7 @@ function App() {
         <Navbar backendURL={backendURL} userType={userType}/>
         <Routes>
           <Route index element={<HomePage userType={userType} backendURL={backendURL} setUserType = {setUserType}/>}/>
-          <Route path="/new-ticket" element={<NewTicketPage/>} />
+          <Route path="/new-ticket" element={<NewTicketPage backendURL={backendURL} categories={categories}/>} />
           <Route path="/view-incidents" element={<ViewIncidentsPage/>} />
           {/*... etc ...*/}
         </Routes>
